37682bb059cb98cf18f4935d08c06a84cf0b3660
Chimdia Abara Obasi <obasichimdiaabara@gmail.com>
2026-03-31 13:36:52 +0100

    Remove host specification in app.run
    Update app.run to use default host

--- app.py
+++ app.py
@@ -671,4 +671,4 @@ def _build_composition_pie(composition):
 # Run
 # ---------------------------------------------------------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8050, debug=False)
+    app.run(debug=False)
